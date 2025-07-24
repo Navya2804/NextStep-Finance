abe45ff94e01e34db30b06c5a9f9132de9ca3a58
VinayTiwari16 <vinaydevopslearning@gmail.com>
2025-07-24 23:13:02 +0530

    initial commit for artifctory set up

--- backend/main.py
+++ backend/main.py
@@ -3,5 +3,6 @@ from database import init_db
 from app import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Default to 8080 if PORT is not set
+    init_db()
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
