3696a4cc4274bd40bd8b5c225aee8303657ded2d
Aditya Gunjal <adityamgunjal1365@gmail.com>
2025-07-24 15:03:02 +0000

    Added summary endpoint for budget

--- backend/routes/budget.py
+++ backend/routes/budget.py
@@ -1,2 +1,14 @@
 from app import app
+from flask import request
+from database.reader import get_transaction_summary
 
+@app.route('/budget/summary', methods=['GET'])
+def summary():
+    # Read 2 string query parameters called lang and timeframe
+    user_id = request.args.get('user_id', 'default_user')
+    lang = request.args.get('lang')
+    timeframe = request.args.get('timeframe')
+
+    print(lang, timeframe)
+
+    return get_transaction_summary(user_id, timeframe)
