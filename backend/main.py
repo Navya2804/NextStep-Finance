d3679531373ed9928599e5406d71185c6c3b6922
VinayTiwari16 <vinaydevopslearning@gmail.com>
2025-07-24 22:32:02 +0530

    initial commit for artifctory set up

--- backend/main.py
+++ backend/main.py
@@ -4,4 +4,4 @@ from app import app
 
 if __name__ == "__main__":
     init_db()
-    app.run(host='0.0.0.0', debug=True, port=8080)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
