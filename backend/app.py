6c34e52a24baf2ae579ef6ff2439dd99ed8ddd31
Aditya-Gunjal <adityamgunjal1365@gmail.com>
2025-07-24 23:28:02 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -12,11 +12,7 @@ CORS(app)
 
 @app.route('/')
 def hello():
-<<<<<<< HEAD
     return 'Hello, World!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-=======
-    return 'Hello, World!'
->>>>>>> 0d9ee1ddf130362b501c786d9ac806606e062203
